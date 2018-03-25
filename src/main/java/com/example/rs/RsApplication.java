11390f3022747ee06eed9ae450b5ee6108441530
Daemon <daemoonn@163.com>
2018-03-25 13:36:59 +0800

    initial

---
+++ src/main/java/com/example/rs/RsApplication.java
@@ -0,0 +1,12 @@
+package com.example.rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsApplication.class, args);
+	}
+}
